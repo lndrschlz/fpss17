4aac78eb97d04a1633447fce8d5b0d624adbc77d
Lenny <inf102143@fh-wedel.de>
2017-09-26 14:59:08 +0200

    Basic functions for Functional Lists

--- 07_functionsAsData/Data/FunctionalList.hs
+++ 07_functionsAsData/Data/FunctionalList.hs
@@ -6,39 +6,44 @@ import qualified Prelude as P
 
 type List a = [a] -> [a]
 
--- ----------------------------------------
-
+-------------------------------------------
 fromList        :: [a] -> List a
-fromList l      = undefined
+fromList l      = \ x -> l ++ x  
 
 toList          :: List a -> [a]
-toList l        = undefined
+toList l        = l []
 
 empty           :: List a
-empty           = undefined
+empty           = fromList []
 
 singleton       :: a -> List a
-singleton e     = undefined
+singleton e     = fromList [e]
 
 -- (:) for functional lists
+-- test case: toList P.$ cons 1 (fromList [2,3,4,5])
 cons            :: a -> List a -> List a
-cons e l        = undefined
+cons e l        = \ x -> [e] ++ l x
 
 -- dual to cons
+-- test case: toList P.$ snoc (fromList [1,2,3,4,5]) 6
 snoc            :: List a -> a -> List a
-snoc l e        = undefined
+snoc l e        = \ x -> l ([e] ++ x)
 
 -- (++) for functional lists
+-- test: toList P.$ append (fromList [1,2,3]) (fromList [4,5,6])
 append          :: List a -> List a -> List a
-append l1 l2    = undefined
+append l1 l2    = l1 . l2
 
 -- like concat for normal lists: foldr (++) []
+-- test: toList P.$ concat [(fromList [1,2,3]),(fromList [4,5,6])]
 concat          :: [List a] -> List a
-concat          = undefined
+concat          = P.foldr (append) empty
 
 -- like map for normal lists: foldr ((:) . f) []
+-- test: toList P.$ map (\ x -> P.show x) (fromList [1,2,3,4])
 map             :: (a -> b) -> List a -> List b
-map f           = undefined
+map f l           = P.foldr (cons . f) empty P.$ l []
+
 
 -- foldr with foldr for normal lists
 foldr           :: (a -> b -> b) -> b -> List a -> b
